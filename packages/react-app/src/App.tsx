df9a99371a65a6aaa7b9307b421678eb0b0e5a9e
Ming-der Wang <mingderwang@gmail.com>
2021-07-04 12:37:22 +0800

    add hello svg filter animation

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -3,6 +3,7 @@ import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 import { Wallet } from "./components/Wallet";
 import { ReactSpringExample } from "./components/ReactSpringExample";
+import { ShowSVG } from "./components/ShowSVG";
 
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
@@ -14,6 +15,7 @@ export const App = () => {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Wallet />
+      <ShowSVG />
       <ReactSpringExample />
     </Web3ReactProvider>
   );
