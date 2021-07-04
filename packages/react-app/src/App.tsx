c895b72927ed297bb1fe3c99f54d61caa5524d46
Ming-der Wang <mingderwang@gmail.com>
2021-07-04 11:46:43 +0800

    ok for adding react-spring with css (not css module)

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -3,6 +3,7 @@ import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 import { Wallet } from "./components/Wallet";
 import { ReactSpringExample } from "./components/ReactSpringExample";
+
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
   library.pollingInterval = 12000;
--- packages/react-app/src/components/ReactSpringExample.tsx
+++ packages/react-app/src/components/ReactSpringExample.tsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { FunctionalComponent, h } from 'preact'
-import { animated, useSpring } from 'react-spring'
+import useMeasure from 'react-use-measure'
+import { useSpring, animated } from '@react-spring/web'
+import './index.css';
 
 export type Props = {}
 
 export const ReactSpringExample: FunctionalComponent<Props> = ({ ...restProps }) => {
-    const props = useSpring({
-        opacity: 1,
-        from: { opacity: 0 },
-    })
-
-    return <animated.h1 style={props}>hello</animated.h1>
-}
+    const [open, toggle] = useState(false)
+    const [ref, { width }] = useMeasure()
+    const props = useSpring({ width: open ? width : 0 })
+  
+    return (
+      <div className='App'>
+        <div ref={ref} className='main' onClick={() => toggle(!open)}>
+          <animated.div className='fill' style={props} />
+          <animated.div className='content'>{props.width.to(x => x.toFixed(0))}</animated.div>
+        </div>
+      </div>
+    )
+  }
